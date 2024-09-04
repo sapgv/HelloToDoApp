afbd77a644b4c9146161ec8252714fec0b1097d1
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-09-04 17:32:36 +0500

    Initial Commit

---
+++ HelloToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloToDoApp
+//
+//  Created by Grigory Sapogov on 04.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
